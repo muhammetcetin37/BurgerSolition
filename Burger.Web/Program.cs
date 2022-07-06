e2ffba0df0e456cbdae08564c912197d23eea946
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-07-06 13:38:28 +0300

    admin erae yazıldı

--- Burger.Web/Program.cs
+++ Burger.Web/Program.cs
@@ -1,7 +1,15 @@
+using Burger.BL.Abstract;
+using Burger.BL.Concrete;
+using Burger.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SqlDbContext>(options => options.UseSqlServer
+                                         (builder.Configuration.GetConnectionString("Burger")));
+builder.Services.AddScoped<IKategoriManager, KategoriManager>();
 
 var app = builder.Build();
 
@@ -16,8 +24,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 app.Run();
