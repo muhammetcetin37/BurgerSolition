e868b6fefa7e145d1721d27fdd1ac843394db4ac
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-07-06 12:42:02 +0300

    kategoriManager sınıfı bl yazıldı

--- Burger.BL/Concrete/KategoriManager.cs
+++ Burger.BL/Concrete/KategoriManager.cs
@@ -5,5 +5,30 @@ namespace Burger.BL.Concrete
 {
     public class KategoriManager : ManagerBase<Kategori>, IKategoriManager
     {
+        //Ayni Kategori isminden bir daha olmasın
+        //1- git db ye bak eger aynı isimde kayıt varsa true gonder
+        public bool IsmiKontrolEt(string KategoriAdi)
+        {
+            var sonuc = base.repository.GetAll(p => p.KategoriAdi == KategoriAdi);
+            if (sonuc != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override int Add(Kategori input)
+        {
+            if (!IsmiKontrolEt(input.KategoriAdi))
+            {
+                return base.Add(input);
+            }
+            else
+            {
+                return -1;
+            }
+        }
     }
 }
--- Burger.BL/Concrete/ManagerBase.cs
+++ Burger.BL/Concrete/ManagerBase.cs
@@ -13,32 +13,32 @@ namespace Burger.BL.Concrete
         {
             repository = new RepositoryBase<T>();
         }
-        public int Add(T input)
+        public virtual int Add(T input)
         {
             return repository.Add(input);
         }
 
-        public int Delete(T input)
+        public virtual int Delete(T input)
         {
             return repository.Delete(input);
         }
 
-        public T Get(int id)
+        public virtual T Get(int id)
         {
             return repository.Get(id);
         }
 
-        public IList<T> GetAll(Expression<Func<T, bool>> filter = null)
+        public virtual IList<T> GetAll(Expression<Func<T, bool>> filter = null)
         {
             return repository.GetAll(null);
         }
 
-        public IQueryable<T> GetAllInclude(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] include)
+        public virtual IQueryable<T> GetAllInclude(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] include)
         {
             return repository.GetAllInclude(filter, include);
         }
 
-        public int Update(T input)
+        public virtual int Update(T input)
         {
             return repository.Update(input);
         }
