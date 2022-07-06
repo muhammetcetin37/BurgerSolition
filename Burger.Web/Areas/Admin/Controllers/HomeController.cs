a0d3576280b2ee3e6bfd9e3c5f02139fa7804e9f
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-07-06 13:47:20 +0300

    AdminLte Admin Area Bolgesine implemnte edildi

---
+++ Burger.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Burger.Web.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
