3696f2f90683d3757352b4840a83aa36303651ac
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-07-07 10:14:31 +0300

    login işlemleri  için alanlar entities e eklendi

--- Burger.Entities/Kullanicilar.cs
+++ Burger.Entities/Kullanicilar.cs
@@ -7,6 +7,11 @@
         public string? TcNo { get; set; }
         public string Email { get; set; }
         public string? Gsm { get; set; }
+        #region kullanici girisi icin alanlar
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        #endregion
         public IList<Adres> Adresler { get; set; }
     }
 }
