92f86999d25fbe1f1194e2e168ea73b958378228
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-07-07 13:49:03 +0300

    tamamlandi

--- Burger.Web/Controllers/DTOs/LoginDTO.cs
+++ Burger.Web/Controllers/DTOs/LoginDTO.cs
@@ -1,17 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Burger.Web.Controllers.DTOs
+namespace Burger.Web.Models.DTOs
 {
     public class LoginDTO
     {
+
         [Required]
-        [MinLength(5, ErrorMessage = "Email Alani En Az 5 Karakter Olmalidir")]
+        [MinLength(5, ErrorMessage = "Email Alani en az 5 karakter olmalidir")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
 
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
+
     }
 }
